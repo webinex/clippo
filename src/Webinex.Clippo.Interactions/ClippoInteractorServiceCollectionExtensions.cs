1dbd8dde849b09179081d2def56ae368888dab97
Webinex Dev <webinex.dev@gmail.com>
2025-03-04 12:13:13 +0300

    Add Interactor as common entrypoint for ASP.NET
    * Fix decoration issue with interactor

--- src/Webinex.Clippo.Interactions/ClippoInteractorServiceCollectionExtensions.cs
+++ src/Webinex.Clippo.Interactions/ClippoInteractorServiceCollectionExtensions.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Webinex.Clippo;
 
 public static class ClippoInteractorServiceCollectionExtensions
 {
-    public static IServiceCollection AddClippoInteractor(this IServiceCollection services)
+    public static IServiceCollection AddClippoInteractor<TMeta, TData>(this IServiceCollection services)
+        where TMeta : class, ICloneable
+        where TData : class, ICloneable
     {
-        services.TryAddScoped(typeof(IClippoInteractor<,>), typeof(IClippoInteractor<,>));
+        services.TryAddScoped(typeof(IClippoInteractor<TMeta, TData>), typeof(ClippoInteractor<TMeta, TData>));
         return services;
     }
 }
